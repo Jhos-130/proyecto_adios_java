3541b7cd30ed99a07b17b55b44e2f56cf8ddf4cd
Jhos-130 <josuecamtor@gmail.com>
2026-05-16 10:56:26 -0600

    Resta de dos numeros en main

--- Adios.java
+++ Adios.java
@@ -1,16 +1,32 @@
 import java.util.Scanner;
 
-public class Adios
-{
-    public static void main(String[] args ){
-        int numero1,numero2;
+public class Adios {
+
+    public static int restar(int numero1, int numero2) {
+        int resultado = numero1 - numero2;
+        return resultado;
+    }
+
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int numero1, numero2;
+
         System.out.println("Escribe el numero 1");
         numero1 = scanner.nextInt();
-         System.out.println("Escribe el numero 2");
+        System.out.println("Escribe el numero 2");
         numero2 = scanner.nextInt();
-         int resultado = numero1 + numero2;
-         System.out.println("La suma es "+resultado);
-         scanner.close();
+
+        int resultado = numero1 + numero2;
+        System.out.println("La suma es " + resultado);
+
+        System.out.println("Escribe el numero 1 para restar");
+        numero1 = scanner.nextInt();
+        System.out.println("Escribe el numero 2 para restar");
+        numero2 = scanner.nextInt();
+
+        int resultadoResta = restar(numero1, numero2);
+        System.out.println("La resta es " + resultadoResta);
+
+        scanner.close();
     }
 }
