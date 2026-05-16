af9d713e34c7fca618fe5f86c9ea0c856bbbe79b
Jhos-130 <josuecamtor@gmail.com>
2026-05-16 11:04:40 -0600

    Clase persona

---
+++ Persona.java
@@ -0,0 +1,65 @@
+public class Persona {
+    private String nombre;
+    private int edad;
+    private String matricula;
+    private String telefono;
+    private String grupo;
+    private String carrera;
+
+    public Persona(String nombre, int edad, String matricula, String telefono, String grupo, String carrera) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.matricula = matricula;
+        this.telefono = telefono;
+        this.grupo = grupo;
+        this.carrera = carrera;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getGrupo() {
+        return grupo;
+    }
+
+    public void setGrupo(String grupo) {
+        this.grupo = grupo;
+    }
+
+    public String getCarrera() {
+        return carrera;
+    }
+
+    public void setCarrera(String carrera) {
+        this.carrera = carrera;
+    }
+}
