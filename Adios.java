d8518a599323840070374a3a1a8c39597d4c1f6a
Jhos-130 <josuecamtor@gmail.com>
2026-05-16 10:09:20 -0600

    Agregar codigo de suma con Scanner

---
+++ Adios.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Adios
+{
+    public static void main(String[] args ){
+        int numero1,numero2;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escribe el numero 1");
+        numero1 = scanner.nextInt();
+         System.out.println("Escribe el numero 2");
+        numero2 = scanner.nextInt();
+         int resultado = numero1 + numero2;
+         System.out.println("La suma es "+resultado);
+    }
+}
